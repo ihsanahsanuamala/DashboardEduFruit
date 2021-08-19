c9106b91ed8577465430e949ae63db0325aa1db2
Ihsan <amalaihsanahsanu@gmail.com>
2021-08-19 08:49:10 +0700

    Done Some Componenet Like Chart,Card and Table

---
+++ script.js
@@ -0,0 +1,49 @@
+// Grafik Penjualan
+const ctx = document.getElementById("semut").getContext('2d');
+      const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["Minggu", "Senin", "Selasa",
+          "Rabu", "Kamis", "Jumat", "Sabtu"],
+          datasets: [{
+            label: 'Last week',
+            backgroundColor: 'rgba(161, 198, 247, 1)',
+            borderColor: 'rgb(47, 128, 237)',
+            data: [3000, 4000, 2000, 5000, 8000, 9000, 2000],
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+              }
+            }]
+          }
+        },
+      });
+
+// Grafik Pengunjung
+const rtx = document.getElementById("rambutan").getContext('2d');
+      const myChart1 = new Chart(rtx, {
+        type: 'line',
+        data: {
+          labels: ["Minggu", "Senin", "Selasa",
+          "Rabu", "Kamis", "Jumat", "Sabtu"],
+          datasets: [{
+            label: 'Last week',
+            backgroundColor: 'rgba(161, 198, 247, 1)',
+            borderColor: 'rgb(47, 128, 237)',
+            data: [10000, 9000, 7000, 12000, 14000, 16000, 8000],
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+              }
+            }]
+          }
+        },
+      });

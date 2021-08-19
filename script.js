fc99242a3efda009503d09f69245a7d7145ecfe7
Ihsan <amalaihsanahsanu@gmail.com>
2021-08-19 14:32:33 +0700

    finish all task

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const ctx = document.getElementById("semut").getContext('2d');
           labels: ["Minggu", "Senin", "Selasa",
           "Rabu", "Kamis", "Jumat", "Sabtu"],
           datasets: [{
-            label: 'Last week',
+            label: 'Minggu Ke -14',
             backgroundColor: 'rgba(161, 198, 247, 1)',
             borderColor: 'rgb(47, 128, 237)',
             data: [3000, 4000, 2000, 5000, 8000, 9000, 2000],
@@ -31,7 +31,7 @@ const rtx = document.getElementById("rambutan").getContext('2d');
           labels: ["Minggu", "Senin", "Selasa",
           "Rabu", "Kamis", "Jumat", "Sabtu"],
           datasets: [{
-            label: 'Last week',
+            label: 'Minggu Ke-14',
             backgroundColor: 'rgba(161, 198, 247, 1)',
             borderColor: 'rgb(47, 128, 237)',
             data: [10000, 9000, 7000, 12000, 14000, 16000, 8000],
